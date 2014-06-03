7610994f56670d1cab304b1eb8a9c52421d64e5f
Ryan Vermooten <rvermooten@gmail.com>
2014-06-03 13:45:30 +0200

    removed test dir

--- test/helpers/trainers_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class TrainersHelperTest < ActionView::TestCase
-end
--- test/models/trainer_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TrainerTest < ActiveSupport::TestCase
-  test "a trainer should enter a first name" do
-    trainer = Trainer.new
-    assert !trainer.save
-    assert !trainer.errors[:first_name].empty?
-  end
-end
